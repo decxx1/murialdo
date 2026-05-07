6bc51054138723d06a9b906b5c0cd784841bdbe1
Damian Cisternas <decxx1@gmail.com>
2026-05-07 11:22:06 -0300

    feat: add Hipopresivos and Stretching activity pages, and implement dynamic dropdown menu component

--- src/components/Menu2.jsx
+++ src/components/Menu2.jsx
@@ -76,6 +76,9 @@ export default function Menu2({dropdownId}) {
                             <a href="/actividades/full-body" className="block px-4 py-2 lg:py-4 hover:bg-light hover:rounded-2xl hover:text-primary">Full body</a>
                         </li>
                         <li>
+                            <a href="/actividades/hipopresivos" className="block px-4 py-2 lg:py-4 hover:bg-light hover:rounded-2xl hover:text-primary">Hipopresivos</a>
+                        </li>
+                        <li>
                             <a href="/actividades/paddle" className="block px-4 py-2 lg:py-4 hover:bg-light hover:rounded-2xl hover:text-primary">Paddle</a>
                         </li>
                         <li>
